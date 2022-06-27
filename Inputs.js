0ee21a8333678e113e2740bad1809c1d40ab67bb
khalil <khalillakhdharatc@gmail.com>
2022-06-27 15:52:31 +0100

    Update Inputs.js

--- Inputs.js
+++ Inputs.js
@@ -46,8 +46,9 @@ class Inputs extends Component {
             input: {
             margin: 15,
             height: 40,
-            borderColor: '#7a42f4',
-            borderWidth: 1
+            borderColor: '#000',
+            borderWidth: 2,
+            
             },
             submitButton: {
             backgroundColor: '#7a42f4',
